36072954234542892e78c7aaca46f1923b21f76c
Dana Jackson <danajackson2@gmail.com>
2020-10-13 17:25:19 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,15 +1,14 @@
 def new_hash
- new_hash = {}
+  {}
 end
 
 def my_hash
-  this_hash = {:state => "Washington", :food => "apples"}
+  {:state => "Washington", :food => "apples"}
 end
 
 def pioneer
-  pioneer_hash = {:name => "Grace Hopper"}
+  {:name => "Grace Hopper"}
 end
-
 
 def id_hash_generator(number)
   hash = {:id => number}
